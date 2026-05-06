021f5eac356f5b62599f7cb1859bb59180ee002a
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-05-06 12:32:19 +0200

    Ejercicios 1, 2 y 3 del bloque 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+//Clase main
+public class Main extends Application {
+    //Metodo start
+    public void start(Stage primaryStage) throws Exception {
+        //Boton con un evento para imprimir por consola
+        Button button = new Button("Haz click aqui");
+        button.setOnAction(event -> {
+            System.out.println("Has pulsado...");
+        });
+        //Escena para crear la ventana con el boton y mostrarla
+        Scene scene = new Scene(button, 300, 200);
+        primaryStage.setTitle("Aplicacion click en boton");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    //Metodo main para ejecutar el programa
+    public static void main(String[] args) {
+        //Metodo launch para lanzar la ventana
+        launch(args);
     }
 }
